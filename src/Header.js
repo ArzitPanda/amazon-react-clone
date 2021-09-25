6d45debef387a1d2756f30faebad2fefebdfc381
ArzitPanda <arzit43.143@gmail.com>
2021-09-25 18:03:25 +0530

    1 binaries uploaded

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import "./Header.css";
+import './Header.css';
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 function Header() {
+  const amazonLogo =
+    'https://www.pngfind.com/pngs/m/70-701602_amazon-prime-logo-png-wwwimgkidcom-the-image-kid.png';
 
-    const amazonLogo="https://www.pinclipart.com/picdir/big/444-4440792_amazon-png-transparent-image-amazon-logo-hd-png.png";
-
-
-    return (
-        <div>
-         <div className="headerContainer">
-             <img  src={amazonLogo} alt="amazon-logo" height="30px" className="logo-amz"/>
-                <input type="text" placeholder="type something" className="input-search"/>
-                <SearchTwoToneIcon/>
-             
-             cart
-         </div>   
+  return (
+    <div>
+      <div className="headerContainer">
+        <img
+          src={amazonLogo}
+          alt="amazon-logo"
+          height="30px"
+          className="logo-amz"
+        />
+        <div className="search__box">
+          <input type="text" className="input-search" />
+          <SearchTwoToneIcon className="search_icon" />
         </div>
-    )
+        <div className="signin">
+          <div className="firsttest">hello user</div>
+          <div className="secondtest">signin</div>
+        </div>
+        <div className="signin">
+          <div className="firsttest">your</div>
+          <div className="secondtest">orders</div>
+        </div>
+        <div className="shop">
+          <ShoppingCartIcon className="ShoppingCart" />
+          <div className="circle">
+            <h1 className="cart-items">2</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
